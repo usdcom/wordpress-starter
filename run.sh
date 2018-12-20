2e9b22bbfbca47e8523df1d9f74e71f8799b7b93
usdcom <awlhy@hotmail.com>
2018-12-20 16:01:42 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,34 +1,6 @@
 #!/bin/bash
 # shellcheck disable=SC1091
 
-if ! sudo mount -a 2>/dev/null; then
-    printf '\e[1;31mERROR:\e[0m %s' \
-    'Container running with improper privileges.
-
-    Be sure your service is configured with the following options:
-    ___
-    services:
-      wordpress:
-        cap_add:
-          - SYS_ADMIN
-        devices:
-          - /dev/fuse
-        # needed on certain cloud hosts
-        security_opt:
-          - apparmor:unconfined
-    ___
-
-    OR (use first option if possible)
-    ___
-    services:
-    wordpress:
-        privileged: true
-    ___
-
-    ' | sed 's/^    //'
-    exit 1
-fi
-
 # Environment
 # ------------
 declare -x TERM="${TERM:-xterm}"
