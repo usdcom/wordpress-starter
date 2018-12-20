ae538b5aaf122003892a2ae0d52b37b478279bb0
usdcom <awlhy@hotmail.com>
2018-12-20 23:36:36 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -61,7 +61,7 @@ rewrite structure:
 
 # Copy plugins
 sudo git clone https://github.com/usdcom/acfp.git /var/www/html/wp-content/plugins
-sudo sed '1 i if($_SERVER["HTTP_X_FORWARDED_PROTO"]=="https"){$_SERVER["HTTPS"]="on";$_SERVER["SERVER_PORT"]=443;}' /var/www/html/wp-config.php
+sudo sed -i '1 a if($_SERVER["HTTP_X_FORWARDED_PROTO"]=="https"){$_SERVER["HTTPS"]="on";$_SERVER["SERVER_PORT"]=443;}' /var/www/html/wp-config.php
 
 # Apache config adustments
 sudo sed -i \
